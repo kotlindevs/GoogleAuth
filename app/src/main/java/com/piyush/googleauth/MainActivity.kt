ea49d59c1fb79cfe2dc4f95b88e7ab173759c735
KotlinCrafter <kotlindevslife@gmail.com>
2024-12-19 19:12:35 +0530

    Add firebase google signIn

--- app/src/main/java/com/piyush/googleauth/MainActivity.kt
+++ app/src/main/java/com/piyush/googleauth/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.piyush.googleauth
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -10,7 +11,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.piyush.googleauth.authentication.BottomSheetAuthenticationRepository
 import com.piyush.googleauth.authentication.BottomSheetAuthenticationViewModel
 import com.piyush.googleauth.authentication.BottomSheetAuthenticationViewModelFactory
@@ -23,6 +32,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel : BottomSheetAuthenticationViewModel
     private lateinit var repository : BottomSheetAuthenticationRepository
     private lateinit var viewModelFactory : BottomSheetAuthenticationViewModelFactory
+
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @Suppress("DEPRECATION")
     override fun onStart() {
         super.onStart()
 
@@ -70,16 +82,68 @@ class MainActivity : AppCompatActivity() {
         binding.bottomSheetGoogleSignIn.setOnClickListener {
             lifecycleScope.launch {
                 val intentSender = repository.googleSignIn()
-                launcher.launch(
+                bottomSheetLauncher.launch(
                     IntentSenderRequest.Builder(
                         intentSender ?: return@launch
                     ).build()
                 )
             }
         }
+
+        binding.firebaseGoogleSignIn.setOnClickListener {
+            signInWithGoogle()
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.oauth_client))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+
     }
 
-    private val launcher = registerForActivityResult(
+    private fun signInWithGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        firebaseLauncher.launch(signInIntent)
+    }
+
+    @Suppress("DEPRECATION")
+    private val firebaseLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result -> if(result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if(task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if(account!= null){
+                updateUi(account)
+            }
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun updateUi(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        Firebase.auth.signInWithCredential(credential).addOnCompleteListener {
+            if(it.isSuccessful){
+                startActivity(
+                    Intent(
+                        this@MainActivity, SecondActivity::class.java
+                    )
+                )
+            }else{
+                Snackbar.make(binding.root,"Sign in failed",Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private val bottomSheetLauncher = registerForActivityResult(
         ActivityResultContracts.StartIntentSenderForResult()
     ){
         result -> if(result.resultCode == RESULT_OK){
